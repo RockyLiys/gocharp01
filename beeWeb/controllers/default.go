4a0d676fb3015ddaef9429e948d3391a1522783d
li.yansong <li.yansong@oyohotels.cn>
2019-11-30 21:36:01 +0800

    .

---
+++ beeWeb/controllers/default.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "baidu.com"
+	c.Data["Email"] = "rockylee.200800@gmail.com"
+	c.TplName = "index.tpl"
+}
--- iot-go/env.sh
+++
@@ -1,1 +0,0 @@
-export GOPATH=/Users/oyo00451/Documents/coding/eclipse-workspace/go-workspace/iot-go
